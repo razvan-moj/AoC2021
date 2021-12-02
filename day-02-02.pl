181f054c05a57d20510d7ad54fcf0e6b3b6b481e
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-12-02 20:41:12 +0000

    day 2 2

---
+++ day-02-02.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+use experimental qw( say switch );
+use Tie::File;
+my $f = 'day-02-01.input';
+tie @data, 'Tie::File', $f or die $!;
+my $h = 0; my $v = 0; my $a = 0;
+for my $steer ( @data ) {
+  my ($where, $howmuch) = $steer =~ /(\w+)\s+(\d+)/;
+  for ($where) {
+    when ($where eq 'forward') { $h += $howmuch; $v += $a*$howmuch; }
+    $a -= $howmuch when $where eq 'up';
+    $a += $howmuch when $where eq 'down';
+  }
+}
+$v = abs($v);
+say "$h $v";
+say $h * $v;
